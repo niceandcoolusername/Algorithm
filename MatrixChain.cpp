0ad95fcaa224531236d19daf375e12169aa0f81c
Shreyas Shubham <33227521+shreyasXV@users.noreply.github.com>
2018-05-08 19:48:42 +0530

    Update MatrixChain.cpp
    Added:Print the place of parenthesis

--- MatrixChain.cpp
+++ MatrixChain.cpp
@@ -2,8 +2,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+void printParenthesis(int i,int j,int n,int *bracket,char &name){
+	if(i==j){
+		cout<<name++;
+		return;}
+	cout<<"(";
+	
+	printParenthesis(i,*(bracket+i*n+j),n,bracket,name);
+	
+	printParenthesis(*(bracket+i*n+j)+1,j,n,bracket,name);
+	cout<<")";
+	
+	
+	
+
+}
+
 int matrixchain(int p[],int n){
 	int m[n][n];
+	int brackets[n][n];
 int i,L,k,j,q;
 	for(i=1;i<n;i++){
 		m[i][i]=0;
@@ -13,12 +31,20 @@ int i,L,k,j,q;
 			j=i+L-1;
 			m[i][j]=INT_MAX;
 			for(k=i;k<=j-1;k++){q=m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j];
-				if(q<m[i][j]) m[i][j]=q;
+				if(q<m[i][j]) {m[i][j]=q;
+					       brackets[i][j]=k;
+					      
+					      }
 
 			}
 		}
 	}
-	return m[1][n-1];
+	
+    char name = 'A';
+ 
+    cout << "Optimal Parenthesization is : ";
+    printParenthesis(1, n-1, n, (int *)brackets, name);
+    cout << "nOptimal Cost is : " << m[1][n-1];
 
 
 }
