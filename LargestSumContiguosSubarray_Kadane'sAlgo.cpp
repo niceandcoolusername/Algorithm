b9177384391a984eb55a7f65604879771c6f3662
Shreyas Shubham <33227521+shreyasXV@users.noreply.github.com>
2018-05-15 04:37:01 +0530

    Added an extra feature to return starting and end indexes!

--- LargestSumContiguosSubarray_Kadane'sAlgo.cpp
+++ LargestSumContiguosSubarray_Kadane'sAlgo.cpp
@@ -1,23 +1,40 @@
-#include<bits/std++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int maxarray(int arr[],int n){
-	int max_ending_here=max_so_far=0;
+class ret{      //multiple_values_return_krne_ke_liye_banaya
+public:
+	int start,end,max_sum;
+};
+
+ret maxarray(int arr[],int size){
+	int max_ending_here=0;
+	int max_so_far=INT_MIN;
+	int start=0,end=0,s=0;
 	for(int i=0;i<size;i++){
-		max_ending_here=max_ending_here+size[i];
-		if(max_so_far<max_ending_here)
-			max_so_far=max_ending_here;
-		if(max_ending_here<0)
-			max_ending_here=0;
+		max_ending_here=max_ending_here+arr[i];
+		if(max_so_far<max_ending_here){
+			start=s;
+			end=i;
+			max_so_far=max_ending_here;}
+		if(max_ending_here<0){
+			s=i+1;
+			max_ending_here=0;}
 
 	}
-	return max_so_far;
+	ret r;
+	r.start=start;
+	r.end=end;
+	r.max_sum=max_so_far;
+	return r;
 }
 
 int main(){
-	int a[]={-2,-3,4,-1,,-2,1,5,-3};
+	int a[]={-2,-3,4,-1,-2,1,5,-3};
 	int n=8;
-	int max_sum=maxarray(a,n);
-	cout << "Maximum contiguous sum is " << max_sum;
+	
+	ret k;
+	k=maxarray(a,n);
+	cout<<"max sum "<<k.max_sum<<endl;
+	cout<<"start and end index : "<<k.start<<" "<<k.end<<endl;
 	return 0;
 }
